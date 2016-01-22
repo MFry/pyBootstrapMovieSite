7530fff800a6f013999257fd6ec2e69dd6212f17
Michal Frystacky <Michal.Frystacky@gmail.com>
2016-01-21 16:53:46 -0900

    Expanded functionality to include the summary, director and stars.

--- media.py
+++ media.py
@@ -2,7 +2,12 @@ __author__ = 'Michal Frystacky'
 
 
 class Movie:
-    def __init__(self, title, poster_image_url, youtube_url):
+    def __init__(self, title, poster_image_url, youtube_url, summary, director, *stars):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = youtube_url
+        self.summary_text = summary
+        self.director = director
+        self.stars = []
+        for star in stars:
+            self.stars.append(star)
